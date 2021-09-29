12913c0ee358250fbc49a6928a4ac85a522847ec
shumbativ <shumbativ@acr.org>
2021-09-29 17:04:33 -0400

    git commit -a -m "Framework setup is completed"

--- ConnectProject/Features/Login.feature.cs
+++ ConnectProject/Features/Login.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace AutomationFramework.Features
+namespace ConnectProject.Features
 {
     using TechTalk.SpecFlow;
     using System;
